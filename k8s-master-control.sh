b3925bb80075d937a9ed47019e53248ebff15da6
sycflash <573997484@qq.com>
2021-11-20 17:32:40 +0800

    删除一些文件

--- k8s-master-control.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-services=(kube-apiserver.service kube-controller-manager.service kube-scheduler.service)
-action=$1
-for service in ${services[@]}
-do
-        systemctl $1 $service
-done
